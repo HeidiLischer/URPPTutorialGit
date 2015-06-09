4d659bc740a7b9b2edf88a673bc1772fcd55ff9a
HeidiLischer <h.lischer@gmx.ch>
2015-06-09 16:08:02 +0200

    work path

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-cd /home/student/Documents/bashScripting
+workPath=/home/student/Documents/bashScripting
+cd ${workPath}
 
 #1. Quality check: FastQC
 ##########################
 mkdir fastQCResults
-/home/student/software/FastQC/fastqc -t 4 -o fastQCResults /home/student/Documents/bashScripting/data/ERR000064_200_1.fastq
-/home/student/software/FastQC/fastqc -t 4 -o fastQCResults /home/student/Documents/bashScripting/data/ERR000064_200_2.fastq
-/home/student/software/FastQC/fastqc -t 4 -o fastQCResults /home/student/Documents/bashScripting/data/ERR000061_500_1.fastq
-/home/student/software/FastQC/fastqc -t 4 -o fastQCResults /home/student/Documents/bashScripting/data/ERR000061_500_2.fastq
+/home/student/software/FastQC/fastqc -t 4 -o fastQCResults ${workPath}/data/ERR000064_200_1.fastq
+/home/student/software/FastQC/fastqc -t 4 -o fastQCResults ${workPath}/data/ERR000064_200_2.fastq
+/home/student/software/FastQC/fastqc -t 4 -o fastQCResults ${workPath}/data/ERR000061_500_1.fastq
+/home/student/software/FastQC/fastqc -t 4 -o fastQCResults ${workPath}/data/ERR000061_500_2.fastq
 
